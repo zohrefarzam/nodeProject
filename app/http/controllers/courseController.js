cc06e247e2f3dbe56084d081dac028661c12ebf3
DESKTOP-KFTJ0J0\Arsh <pouryarezaee76@gmail.com>
2022-01-22 17:18:28 +0330

    course detail

--- app/http/controllers/courseController.js
+++ app/http/controllers/courseController.js
@@ -15,7 +15,25 @@ class courseController extends controller {
         },
       },
     ]);
-    res.render("home/single-course", { course });
+    let canUserUse = await this.canUse(req, course);
+    res.render("home/single-course", { course,canUserUse });
+  }
+  async canUse(req, course) {
+    let canUse = false;
+    if (req.isAuthenticated()) {
+      switch (course.type) {
+        case "vip":
+          canUse = req.user.isVip();
+          break;
+        case "cash":
+          canUse = req.user.checkLearning(course);
+          break;
+        default:
+          canUse = true;
+          break;
+      }
+    }
+    return canUse;
   }
 }
 
--- app/models/user.js
+++ app/models/user.js
@@ -1,47 +1,61 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const uniqueString = require('unique-string')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const uniqueString = require("unique-string");
 
-const userSchema = mongoose.Schema({
-    name : { type : String , required : true },
-    admin : { type : Boolean ,  default : 0 },
-    email : { type : String , unique : true  ,required : true},
-    password : { type : String ,  required : true },
-    rememberToken : { type : String , default : null },
-} , { timestamps : true , toJSON : { virtuals : true } });
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    admin: { type: Boolean, default: 0 },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    rememberToken: { type: String, default: null },
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
 
-userSchema.pre('save' , function(next) {
-    let salt = bcrypt.genSaltSync(15);
-    let hash = bcrypt.hashSync(this.password , salt);
+userSchema.pre("save", function (next) {
+  let salt = bcrypt.genSaltSync(15);
+  let hash = bcrypt.hashSync(this.password, salt);
 
-    this.password = hash;
-    next();
+  this.password = hash;
+  next();
 });
 
-userSchema.pre('findOneAndUpdate' , function(next) {
-    let salt = bcrypt.genSaltSync(15);
-    let hash = bcrypt.hashSync(this.getUpdate().$set.password , salt);
+userSchema.pre("findOneAndUpdate", function (next) {
+  let salt = bcrypt.genSaltSync(15);
+  let hash = bcrypt.hashSync(this.getUpdate().$set.password, salt);
 
-    this.getUpdate().$set.password = hash;
-    next();
+  this.getUpdate().$set.password = hash;
+  next();
 });
 
-userSchema.methods.comparePassword = function(password) {
-    return bcrypt.compareSync(password , this.password);
-}
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
-userSchema.methods.setRememberToken = function(res) {
-    const token = uniqueString();
-    res.cookie('remember_token' , token , { maxAge : 1000 * 60 * 60 * 24 * 90 , httpOnly : true , signed :true});
-    this.update({ rememberToken : token } , err => {
-        if(err) console.log(err);
-    });
-}
+userSchema.methods.setRememberToken = function (res) {
+  const token = uniqueString();
+  res.cookie("remember_token", token, {
+    maxAge: 1000 * 60 * 60 * 24 * 90,
+    httpOnly: true,
+    signed: true,
+  });
+  this.update({ rememberToken: token }, (err) => {
+    if (err) console.log(err);
+  });
+};
 
-userSchema.virtual('courses' , {
-    ref : 'Course',
-    localField : '_id',
-    foreignField : 'user'
-})
+userSchema.virtual("courses", {
+  ref: "Course",
+  localField: "_id",
+  foreignField: "user",
+});
+userSchema.methods.isVip = function () {
+  return true;
+};
 
-module.exports = mongoose.model('User' , userSchema);
+userSchema.methods.checkLearning = async function (course) {
+    return true;
+  };
+
+module.exports = mongoose.model("User", userSchema);
