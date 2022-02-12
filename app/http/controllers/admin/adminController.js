3bd55dbe9844abdb6d0651cdabd4669cebbf2f49
zohrefarzam <zohrefarzam@gmail.com>
2022-02-12 16:14:44 +0330

    ck editor & upload image

--- app/http/controllers/admin/adminController.js
+++ app/http/controllers/admin/adminController.js
@@ -4,7 +4,14 @@ class courseController extends controller {
     index(req , res) {
         res.render('admin/index');
     }
-
+    uploadImage(req, res) {
+        let image = req.file;
+        res.json({
+            "uploaded" : 1,
+            "fileName" : image.originalname,
+            "url" : `${image.destination}/${image.filename}`.substring(8)
+        });
+    }
 }
 
 module.exports = new courseController();
